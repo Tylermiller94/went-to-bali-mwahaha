39f18821ae8e309734745a319e06d7e87a6260d0
tylermiller94 <tylermiller94@gmail.com>
2017-12-20 04:12:21 -0800

    Add Password validations to ensure they are appropriate length, flash messages broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :orders
+
+  has_secure_password
+  validates :password, length: { minimum: 5 }
 
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
